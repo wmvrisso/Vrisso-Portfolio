981d2a48f3ecd1fb86c7dd55ac1e2fcd65b14f27
wmvrisso <wmvrisso@gmail.com>
2025-03-23 18:54:39 -0700

    updated navbar styling

---
+++ vrisso-portfolio/v-portfolio/src/components/Hero/hero.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles/hero.css";
+
+const Hero = () => {
+  return <div className="hero"></div>;
+};
+
+export default Hero;
--- vrisso-portfolio/v-portfolio/src/components/navbar.jsx
+++ vrisso-portfolio/v-portfolio/src/components/navbar.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import "./navbar.css";
+import logo from "../assets/images/vrisso_logo.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <img src={logo} alt="" />
+
+      <ul className="nav-menu ${menuOpen ? 'active' : ''}">
+        <li>About Me</li>
+        <li>Portfolio</li>
+        <li>Resume</li>
+        <li>Contact</li>
+      </ul>
+      <div className="nav-connect">Let's Connect</div>
+    </div>
+  );
 };
 
 export default Navbar;
