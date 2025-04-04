2f6ccc22ab487cc201edc68559135df19ac5ee2f
wmvrisso <wmvrisso@gmail.com>
2025-04-03 20:00:05 -0700

    updated mywork_data.js file

--- vrisso-portfolio/v-portfolio/src/assets/mywork_data.js
+++ vrisso-portfolio/v-portfolio/src/assets/mywork_data.js
@@ -7,19 +7,22 @@ const mywork_data = [
     w_no: 1,
     w_name: "Open Weather API",
     w_img: project1_img,
-    github_link: "https://github.com/yourusername/open-weather-api", // Replace with actual GitHub link
+    github_link: "https://github.com/wmvrisso/openWeather_api.git",
+    deployed_link: "https://openweather-api.onrender.com",
   },
   {
     w_no: 2,
     w_name: "Candidate Search App",
     w_img: project2_img,
-    github_link: "https://github.com/yourusername/candidate-search-app", // Replace with actual GitHub link
+    github_link: "https://github.com/wmvrisso/candidate-search-app.git",
+    deployed_link: "https://candidate-search-app-lr96.onrender.com",
   },
   {
     w_no: 3,
     w_name: "Kanban Board App",
     w_img: project3_img,
-    github_link: "https://github.com/yourusername/kanban-board-app", // Replace with actual GitHub link
+    github_link: "https://github.com/wmvrisso/Kanpan-Board-App.git",
+    deployed_link: "https://kanpan-board-app.onrender.com",
   },
 ];
 
