5f0f6d6ae8c6a2477a0aea69a99d17681c7320c2
wmvrisso <wmvrisso@gmail.com>
2025-06-02 20:21:34 -0700

    added new resume pdf file

--- vrisso-portfolio/v-portfolio/src/components/hero.jsx
+++ vrisso-portfolio/v-portfolio/src/components/hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../components/hero.css";
 import profile_img from "../assets/images/profile_will.png";
-import resume_pdf from "../assets/files/Will-Vrisso-Resume.pdf"; // Import the resume file
+import resume_pdf from "../assets/files/Vrisso-Resume-25.pdf"; // Import the resume file
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Hero = () => {
@@ -22,7 +22,7 @@ const Hero = () => {
           </AnchorLink>
         </div>
         <div className="hero-resume">
-          <a href={resume_pdf} download="Will_Vrisso_Resume.pdf">
+          <a href={resume_pdf} download="Vrisso-Resume-25.pdf">
             Resume
           </a>
         </div>
